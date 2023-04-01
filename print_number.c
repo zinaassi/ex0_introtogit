29ebad3eb73502e4aae6929c09d662b148edee09
Zina Assi <zina.assi@csl3.cs.technion.ac.il>
2023-04-01 12:18:05 +0300

    fixed the bug

--- print_number.c
+++ print_number.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int course_number[6] = {4, 2, 1, 4, 3, 2};
 int main() {
-	for(int i = 6; i > 0; i--) {
+	int course_number[6] = {4, 2, 1, 4, 3, 2};
+	for(int i = 5; i > 0; i--) {
 		printf("%d", course_number[i]);
 	}
 	printf("\n");
